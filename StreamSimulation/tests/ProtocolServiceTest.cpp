921d5b6e39c79bfca291b74e27df978474ec6f97
Sang Park <sangpark83@gmail.com>
2024-10-11 21:05:43 +0300

    Update unit test

--- StreamSimulation/tests/ProtocolServiceTest.cpp
+++ StreamSimulation/tests/ProtocolServiceTest.cpp
@@ -1,11 +1,19 @@
 #include <gtest/gtest.h>
 #include <ProtocolService.hpp>
 
-TEST(ProtocolServiceTest, DemoProtocolServiceTest) {
+TEST(ProtocolServiceTest, DemoQueuedProtocolServiceTest) {
     StreamSim::Net::DemoProtocolServiceQueued service(4, 1);
     service.Run();
     service.Shutdown();
 
     EXPECT_EQ(service.GetNumDecodeBufferElements(), 0);
+    EXPECT_EQ(service.GetNumDecodedBufferElements(), 0);
+}
+
+TEST(ProtocolServiceTest, DemoPooledProtocolServiceTest) {
+    StreamSim::Net::DemoProtocolServicePooled service(4, 1);
+    service.Run();
+    service.Shutdown();
+
     EXPECT_EQ(service.GetNumDecodedBufferElements(), 0);
 }
